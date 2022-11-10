5364af8da12406bff1fd1433a5a0b2799db1e719
Erhan AKÇA <erhan-akca@hotmail.com>
2022-11-10 20:18:56 +0300

    successes added to reponses

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
@@ -20,34 +21,30 @@ class AuthController extends Controller
     /**
      * Get a JWT via given credentials.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function login(Request $request): \Illuminate\Http\JsonResponse
+    public function login(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+            return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
         }
         if (! $token = auth()->attempt($validator->validated())) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['success' => false, 'error' => 'Unauthorized'], 401);
         }
         return $this->createNewToken($token);
     }
     /**
      * Register a User.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function register(Request $request): \Illuminate\Http\JsonResponse
+    public function register(Request $request): JsonResponse
     {
         $data = $request->all();
-        $name = $data['name'];
-        $data['name'] = mb_convert_case($name, MB_CASE_TITLE, "UTF-8");
-        $surname = $data['surname'];
-        $data['surname'] = mb_strtoupper($surname, "UTF-8");
 
         $validator = Validator::make($data, [
             'name' => 'required|string|between:2,100',
@@ -56,7 +53,7 @@ class AuthController extends Controller
             'password' => 'required|string|confirmed|min:6',
         ]);
         if($validator->fails()){
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json(['errors' => $validator->errors()->toJson(), 'success' => false], 400);
         }
         $user = User::create(array_merge(
             $validator->validated(),
@@ -64,6 +61,7 @@ class AuthController extends Controller
         ));
         return response()->json([
             'message' => 'User successfully registered',
+            'success' => true,
             'user' => $user
         ], 201);
     }
@@ -71,40 +69,41 @@ class AuthController extends Controller
     /**
      * Log the user out (Invalidate the token).
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function logout(): \Illuminate\Http\JsonResponse
+    public function logout(): JsonResponse
     {
         auth()->logout();
-        return response()->json(['message' => 'User successfully signed out']);
+        return response()->json(['success' => true, 'message' => 'User successfully signed out']);
     }
     /**
      * Refresh a token.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function refresh(): \Illuminate\Http\JsonResponse
+    public function refresh(): JsonResponse
     {
         return $this->createNewToken(auth()->refresh());
     }
     /**
      * Get the authenticated User.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
     public function userProfile() {
-        return response()->json(auth()->user());
+        return response()->json(['success' => true, 'user' => auth()->user()]);
     }
     /**
      * Get the token array structure.
      *
      * @param  string $token
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    protected function createNewToken($token): \Illuminate\Http\JsonResponse
+    protected function createNewToken($token): JsonResponse
     {
         return response()->json([
+            'success' => true,
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
