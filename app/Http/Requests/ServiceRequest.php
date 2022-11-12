7df4b9c6cb0e5e18d0e731ad8b38b62bb966ba5d
Erhan AKÇA <erhan-akca@hotmail.com>
2022-11-12 22:49:52 +0300

    request fixed for service

--- app/Http/Requests/ServiceRequest.php
+++ app/Http/Requests/ServiceRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Validator;
 
 class ServiceRequest extends FormRequest
 {
@@ -24,9 +28,28 @@ class ServiceRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'price' => 'required',
-            'cost' => 'required'
+            'name' => 'required|string',
+            'price' => 'required|integer',
+            'cost' => 'required|integer|lt:price'
         ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is Required!',
+            'price.required' => 'Price is Required!',
+            'cost.required' => 'Cost is Required!',
+        ];
+    }
+
+    public function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' =>$validator->errors()->first(),
+            'data' => null,
+        ], 422));
+
     }
 }
